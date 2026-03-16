15341d925c7f3dc7c2af5c57fbeff8864f6558e2
Pablo Esteban <termigol82@gmail.com>
2026-03-16 23:07:48 +0100

    pre deploy

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,29 @@ export default function App() {
   }, [dark]);
 
   /* -----------------------------
+     CONVERTIR VAPID KEY
+  ----------------------------- */
+
+  function urlBase64ToUint8Array(base64String) {
+
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+
+    const base64 = (base64String + padding)
+      .replace(/-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+  }
+
+  /* -----------------------------
      SUSCRIPCIÓN A PUSH
   ----------------------------- */
 
@@ -35,12 +58,18 @@ export default function App() {
       console.log("Push permission denied");
       return null;
     }
+
     console.log("SUBSCRIBING TO PUSH");
+
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
-      applicationServerKey: import.meta.env.VITE_VAPID_PUBLIC_KEY
+      applicationServerKey: urlBase64ToUint8Array(
+        import.meta.env.VITE_VAPID_PUBLIC_KEY
+      )
     });
+
     console.log("SUBSCRIPTION CREATED", subscription);
+
     return subscription;
   }
 
