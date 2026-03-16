61604269132effec60af753f3e352158734f5d5d
Pablo Esteban <termigol82@gmail.com>
2026-03-17 00:26:33 +0100

    pre deploy

--- public/sw.js
+++ public/sw.js
@@ -2,27 +2,22 @@ self.addEventListener("push", function (event) {
 
   console.log("PUSH RECEIVED");
 
-  let data = {
-    title: "Timeo",
-    body: "Nueva notificación"
-  };
+  let data = {};
 
   if (event.data) {
-    try {
-      data = event.data.json();
-    } catch (e) {
-      console.log("Push data error", e);
-    }
+    data = event.data.json();
   }
 
+  const title = data.title || "Timeo";
+
+  const options = {
+    body: data.body || "Nueva notificación",
+    icon: "/icon-192.png",
+    badge: "/icon-192.png"
+  };
+
   event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "/icon-192.png",
-      badge: "/icon-192.png",
-      vibrate: [200, 100, 200],
-      tag: "timeo-notification"
-    })
+    self.registration.showNotification(title, options)
   );
 
 });
