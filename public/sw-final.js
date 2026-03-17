b2382a1d184283d260e81cd491ea213154c6f2de
Pablo Esteban <termigol82@gmail.com>
2026-03-17 14:04:14 +0100

    pre deploy

---
+++ public/sw-final.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', event => {
+  console.log('SW instalado');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('SW activado');
+  return self.clients.claim();
+});
+
+self.addEventListener('push', function (event) {
+
+  console.log('📩 PUSH EVENT');
+
+  let data = {};
+
+  try {
+    if (event.data) {
+      data = event.data.json();
+    }
+  } catch (e) {
+    console.log('❌ JSON ERROR', e);
+  }
+
+  console.log('📦 DATA RECIBIDA:', data);
+
+  const title = data.title || 'TIMEO';
+  const body = data.body || 'SIN CONTENIDO';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: '/icon-192.png',
+    })
+  );
+});
