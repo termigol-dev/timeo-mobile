d8309181e0dd64b903649fde423ada5379d18311
Pablo Esteban <termigol82@gmail.com>
2026-03-17 08:49:08 +0100

    pre deploy

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,7 @@
-self.addEventListener("push", function (event) {
+self.addEventListener("push", event => {
 
-  console.log("PUSH EVENT RECEIVED");
-
-  let data = { title: "Timeo", body: "Push recibido" };
-
-  if (event.data) {
-    try {
-      data = event.data.json();
-    } catch {
-      data.body = event.data.text();
-    }
-  }
-
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "/icon-192.png",
-      badge: "/icon-192.png"
-    })
-  );
+  self.registration.showNotification("DEBUG", {
+    body: "El evento push llegó al SW"
+  });
 
 });
